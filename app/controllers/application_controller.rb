dc8fd4e263a7cb0323b3bf5b4eabb1c16b129874
Jose Fletes <joseangelfletes@gmail.com>
2022-10-31 21:38:53 -0700

    commit tree 7cacaa969ed78adb2ae86952d4a9727e61f4c3ff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(only: [:name], include: {baked_goods: { only: [:name, :price]}})
+  end
+
+  get '/baked_goods/by_price' do
+    bakeries = BakedGood.all.order(price: :desc)
+    bakeries.to_json
+  end
+  
+  get '/baked_goods/most_expensive' do
+    bakeries = BakedGood.all.order(price: :desc).first
+    bakeries.to_json(only: [:name, :price])
+  end
 
 end
